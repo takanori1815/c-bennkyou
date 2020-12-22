be0aa5b08ba51b4795c5389ae15ba10fb63a8c35
takanori1815 <takakki2810@gmail.com>
2020-12-22 19:38:15 +0900

    konnnamon

---
+++ p.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int x, a, b;
+  cin >> x >> a >> b;
+
+  // 1.の出力
+  x++;
+  cout << x << endl;
+
+  // ここにプログラムを追記
+  cout << x + a + b << endl;
+  cont << x + a + b
+}
