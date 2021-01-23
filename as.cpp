4c8c2cd2909cb49d878a5698bb01ec2cc18b3d04
takanori1815 <takakki2810@gmail.com>
2021-01-23 21:05:37 +0900

    tabun

---
+++ as.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  char a,b,c;
+  cin >> a>>b >>c;
+  if(a == b && b == c){
+    cout << "Won" << endl;
+  } else{
+    cout << "Lost" << endl;
+  }
+}
