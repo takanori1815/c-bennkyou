d1ddbb78fb9460a3b3e4514cd80cc9680ebaf8d9
takanori <takakki2810@gmail.com>
2021-09-18 01:18:50 +0900

    ABC217

---
+++ atcoder/ABC217/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  if (s.at(n-1) == 'o')
+  {
+    cout << "Yes" << endl;
+  }
+  else
+  {
+    cout << "No" << endl;
+  }
+}
